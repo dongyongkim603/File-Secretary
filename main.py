79ad2584021a2b6dd41f390c4128d4214ff9a04d
John <dongyongkim8@gmail.com>
2021-11-25 22:26:24 -0500

    Added fucntion to read in directory contents

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os
 
+class Main:
 
-def load_folder():
-    path = input("Enter the path of the folder: ")
-    print(path)
-    file_array = os.listdir(path)
-    
+    def load_folder(input_path):
+        if input_path is None:
+            path = input("Enter the path of the folder: ")
+            print(path)
+        else:
+            path = input_path
+        try:
+            file_array = os.listdir(path)
+            for file in file_array:
+                if os.path.isfile(path + "\\" + file):
+                    print("FILE: " + file)
+                else:
+                    print("FOLDER: " + file)
+        except OSError:
+            print("This is not a valid folder path")
+        finally:
+            print("Ending file read")
 
-load_folder()
+    load_folder(None)
